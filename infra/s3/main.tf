8cc1e35aa1774e9f42aa246baf9d3a002c982c90
fabio <>
2025-03-04 02:34:02 -0300

    teste

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,3 +1,8 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+
+  tags = {
+    Environment = var.env_name
+    Project     = "TerraformPipeline"
+  }
 }
--- infra/s3/variables.tf
+++ infra/s3/variables.tf
@@ -2,3 +2,10 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+
+
+variable "env_name" {
+  description = "The name of the environment"
+  type        = string
+  
+}
