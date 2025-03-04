f121cd675e919dad8b2db6252d62afc4514049de
fabio <>
2025-03-04 18:38:53 -0300

    removed unused variable on s3 module

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,4 @@
 module "s3" {
   source      = "./s3"
   bucket_name = var.bucket_name
-  env_name    = var.env_name
 }
--- infra/s3/variables.tf
+++ infra/s3/variables.tf
@@ -2,10 +2,3 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
-
-
-variable "env_name" {
-  description = "The name of the environment"
-  type        = string
-
-}
